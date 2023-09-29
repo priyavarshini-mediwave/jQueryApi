e0ae113d64b992aba4474ccb2b04414f0bae050c
priyavarshini-mediwave <priyavarshini@mindwaveventures.com>
2023-09-29 13:55:54 +0530

    Complete

--- app/script.js
+++ app/script.js
@@ -1,8 +1,10 @@
 let dogs = [];
 let dogsKeys = [];
+let imgUrl = "";
 function getAllDogsFromApi() {
   // https://dog.ceo/api/breeds/list/all
   const url = "https://dog.ceo/api/breeds/list/all";
+  showLoading();
   $.ajax(url, {
     method: "GET",
     success: function (resp) {
@@ -14,6 +16,7 @@ function getAllDogsFromApi() {
       dogsKeys = Object.keys(dogs.message);
       //console.log(dogsKeys);
       appendtodogSelect(dogsKeys);
+      hideLoading();
     },
 
     error: function () {
@@ -28,11 +31,21 @@ function getAllDogsFromApi() {
 function getRandomImageOfDog(dogBreed) {
   // https://dog.ceo/api/breed/affenpinscher/images/random
   const url = `https://dog.ceo/api/breed/${dogBreed}/images/random`;
+  showLoading();
   $.ajax(url, {
     method: "GET",
     success: function (resp) {
       console.log("Api request success");
       console.log(resp);
+      let imgMesssage = resp;
+      //console.log(imgMesssage.message)
+
+      imgUrl = imgMesssage.message;
+      console.log(imgUrl);
+      displayName(dogBreed);
+      displayImage(imgUrl);
+      hideLoading();
+      clearSelect();
     },
     error: function () {
       console.log("Api request error");
@@ -55,4 +68,41 @@ function appendtodogSelect(dogsKeys) {
   }
 }
 getAllDogsFromApi();
-let btnSubmit=
+let btnSubmit = $("#btnSubmit");
+btnSubmit.click(function () {
+  //alert("hi");
+  const selectInput = $("#dogSelect");
+  let dogType = selectInput.val();
+  console.log(dogType);
+
+  getRandomImageOfDog(dogType);
+  clearSelect();
+});
+
+function displayImage(imgUrl) {
+  const dogImage = $("#dogImage");
+  //let imgUrl1 = imgUrl;
+  dogImage.attr("src", imgUrl);
+}
+function showLoading() {
+  const loadpara = $("#load-para");
+  loadpara.show();
+}
+function hideLoading() {
+  const loadpara = $("#load-para");
+  loadpara.hide();
+}
+function clearSelect() {
+  const selectInput = $("#dogSelect");
+  selectInput.val("");
+}
+function displayName(dogBreed) {
+  $("#dogName").remove();
+  let dogName = $("<p>");
+  dogName.attr("class", "dogName");
+  dogName.attr("id", "dogName");
+  dogName.text(dogBreed);
+  const dogImage = $("#dogImage");
+  dogName.insertBefore(dogImage);
+}
+
